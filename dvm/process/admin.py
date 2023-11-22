3b853ca57c9b12e7ca8043acb97ab9e7a9a49aa8
Nishant <nishantdewangan2002@gmail.com>
2023-11-22 11:53:58 +0530

    final1

--- dvm/process/admin.py
+++ dvm/process/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(voter_Details)
+admin.site.register(oppositions)
+admin.site.register(voteends)
--- dvm/process/apps.py
+++ dvm/process/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class VerifyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'process'
+
